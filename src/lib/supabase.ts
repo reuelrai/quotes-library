df6f92de8e37b3e4289157dabffadd3be7b06b05
Reuel Rai <reuelrai2001@gmail.com>
2025-06-15 22:30:11 +0545

    minor changes

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = "https://upgxpasuoyqerftwjnzf.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVwZ3hwYXN1b3lxZXJmdHdqbnpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAwMDE0OTIsImV4cCI6MjA2NTU3NzQ5Mn0.qDanqKlancuJir0meBuw18hKABgUZZMRyEcv2907Kfc"
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
